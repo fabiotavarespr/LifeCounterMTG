e68f91d49d206d08e760386d8b284096d87b9944
Fabio Tavares P Rego <fabiotavarespr@github.com>
2018-05-22 19:01:06 -0300

    Initial commit

---
+++ app/src/main/java/fabiotavares/com/br/lifecountermtg/MainActivity.java
@@ -0,0 +1,13 @@
+package fabiotavares.com.br.lifecountermtg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
